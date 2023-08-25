38f3b42463b6112f537ebb10ef6923fe0a4e43bf
bodzia <43539600+bodzia@users.noreply.github.com>
2023-08-25 15:15:48 +0200

    Update TC1.js

--- test/TC1.js
+++ test/TC1.js
@@ -3,22 +3,24 @@ const require = createRequire(import.meta.url);
 const assert = require('assert');
 const expect = require('chai').expect;
 import {putOnRack} from "../app/app.js";
-const inputData = require('../testData/testData.json');
+const inputDataTC1 = require('../testData/testDataTC1.json');
 
 describe('Test tubes ', function () {
         it(' are successfully placed on correct rack', function () {
-            let testData = inputData[0];
+            let testData = inputDataTC1[0];
             let result = putOnRack(testData);
             let expectedResult = 'Test tube placed on rack 1'
             assert.equal(result, expectedResult);
         })
         it('return error when invalid parameter is supplied', function () {
-            let result = putOnRack(123)
+            let testData = inputDataTC1[1];
+            let result = putOnRack(testData);
             let expectedResult = 'Ups...an error occured!';
             expect(result, expectedResult);
         })
-        it('return error when invalid parameter is supplied', function () {
-            let result = putOnRack(123)
+        it('return error when no rack ae unavailable', function () {
+            let testData = inputDataTC1[2]
+            let result = putOnRack(testData);
             assert.equal(result, 'Ups...an error occured!');
         });
 });
